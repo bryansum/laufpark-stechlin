e6b29a1f8d96293d783ec4bdd49c313fe1b03dbd
bryansum <bsummersett@gmail.com>
2017-12-06 17:21:46 +0100

    Add count here also to demonstrate "Incremental" capabilities

--- MacPlayground.playground/Pages/Incremental Explanation.xcplaygroundpage/Contents.swift
+++ MacPlayground.playground/Pages/Incremental Explanation.xcplaygroundpage/Contents.swift
@@ -50,13 +50,14 @@ extension I {
 
 struct State: Equatable {
   var arr: ArrayWithChanges<Int>
+  var count: Int
 
   static func == (lhs: State, rhs: State) -> Bool {
     return lhs.arr == rhs.arr
   }
 }
 
-var state = Input<State>(State(arr: [1, 2, 3]))
+var state = Input<State>(State(arr: [1, 2, 3], count: 0))
 
 let d = state.i.observe {
     print("state: \($0)")
@@ -67,7 +68,13 @@ let d2 = arr.observe(current: { current in
 }) { change in
   print("change: \(change)")
 }
+let d3 = state[\.count].observe {
+  print("count: \($0)")
+}
 
 state.change { $0.arr.change(.insert(4, at: 3)) }
-state.change { $0.arr.change(.insert(5, at: 4)) }
+state.change {
+  $0.count = 2
+  $0.arr.change(.insert(5, at: 4))
+}
 
